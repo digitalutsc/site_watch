f9ad4676edb7d53c81a9ef6a4d5c486168c8b4c0
Hassan El-Sheikha <hmelsheikha@gmail.com>
2023-06-14 13:34:44 -0400

    remove versioning from setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name='SiteWatch',
-    version='1.0',
     author='Hassan El-Sheikha',
     description='An Islandora site monitoring tool',
     url='https://github.com/digitalutsc/site_watch',
